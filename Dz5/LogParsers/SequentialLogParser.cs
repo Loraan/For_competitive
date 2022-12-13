bbfa30e3a37468cf838ed103fe8b8fff74cf7d45
Loraan <bfire2066@gmail.com>
2022-12-14 00:24:09 +0500

    5 дз для кирилла <3

---
+++ Dz5/LogParsers/SequentialLogParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LogParsing.LogParsers
+{
+    public class SequentialLogParser : ILogParser 
+    {
+        private readonly FileInfo _file;
+        private readonly Func<string, string?> _tryGetIdFromLine;
+
+        public SequentialLogParser(FileInfo file, Func<string, string?> tryGetIdFromLine)
+        {
+            this._file = file;
+            this._tryGetIdFromLine = tryGetIdFromLine;
+        }
+        
+        public string[] GetRequestedIdsFromLogFile()
+        {
+            var lines = File.ReadLines(_file.FullName);
+            return lines
+                .Select(_tryGetIdFromLine)
+                .Where(id => id != null)
+                .ToArray();
+        }
+    }
+}
